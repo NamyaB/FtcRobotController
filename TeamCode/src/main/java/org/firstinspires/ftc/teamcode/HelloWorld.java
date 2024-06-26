cb4afa262067b032e3521c058796ae6570b18e9f
Namya <bhargava.namya@gmail.com>
2024-06-25 23:23:57 -0700

    Update HelloWorld.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -3,11 +3,25 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class HelloWorld extends OpMode {
-    // called for when driver presses INIT on driver statiom
+    enum State {
+        START,
+        SECOND_STEP,
+        DONE
+    }
+
+    State state = State.START;
+    double lastTime;
+    // called for when driver presses INIT on driver station
     @Override
     public void init() {
-        String myName = "Namya Bhargava";
-        telemetry.addData("Hello", myName);
+        telemetry.addData("Hello ", "World");
+    }
+
+    @Override
+    public void start() {
+        state = State.START;
+        resetRuntime();
+        lastTime = getRuntime();
     }
 
     // called repeatedly (50 times per second) until user presses PAUSE
@@ -26,23 +40,26 @@ public class HelloWorld extends OpMode {
         else if (gamepad1.b) telemetry.addData("B button", " is pressed");
         else telemetry.addData("Neither A nor B button", " is pressed");
 
-        telemetry.addData("sum of triggers", gamepad1.right_trigger+gamepad1.left_trigger);
+        telemetry.addData("sum of triggers", gamepad1.right_trigger + gamepad1.left_trigger);
 
-        if (!gamepad1.a) {
-            forwardSpeed *= 0.5;
-        }
-        telemetry.addData("forward speed", forwardSpeed);
-
-        if (gamepad1.a) {
-            telemetry.addData("right stick x", gamepad1.right_stick_y);
-            telemetry.addData("left stick x", gamepad1.left_stick_y);
-            telemetry.addData("right stick y", gamepad1.right_stick_x);
-            telemetry.addData("left stick y", gamepad1.left_stick_x);
-        } else {
-            telemetry.addData("right stick x", gamepad1.right_stick_x);
-            telemetry.addData("left stick x", gamepad1.left_stick_x);
-            telemetry.addData("right stick y", gamepad1.right_stick_y);
-            telemetry.addData("left stick y", gamepad1.left_stick_y);
-        }
+        telemetry.addData("State", state);
+        telemetry.addData("Runtime", getRuntime());
+        telemetry.addData("Time in State", getRuntime() - lastTime);
+        switch (state) {
+            case START:
+                if (getRuntime() >= 3.0) {
+                    state = State.SECOND_STEP;
+                    lastTime = getRuntime();
+                }
+                break;
+            case SECOND_STEP:
+                if (getRuntime() >= lastTime + 3.0) {
+                    state = State.DONE;
+                }
+                lastTime = getRuntime();
+                break;
+            default:
+                telemetry.addData("Auto", "Finished");
+            }
     }
 }
